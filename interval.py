04f9271a8d20e9e050f45b4a3fabda307be4debb
averykhoo <averykhoo@gmail.com>
2020-07-21 22:19:29 +0800

    hopefullt the generalized apply func works

--- interval.py
+++ interval.py
@@ -10,7 +10,7 @@ from typing import Union
 class Interval:
     # https://oeis.org/wiki/Intervals
     start: Real
-    start_open: bool  # todo: this is slightly broken since endpoints need to be comparable
+    start_open: bool
     end: Real
     end_closed: bool
 
@@ -23,20 +23,20 @@ class Interval:
         return not self.end_closed
 
     @property
-    def start_tuple(self) -> Tuple[Real, int]:
-        return self.start, 1 if self.start_open else 0
-
-    @property
-    def end_tuple(self) -> Tuple[Real, int]:
-        return self.end, 0 if self.end_closed else -1
-
-    @property
     def is_degenerate(self) -> bool:
         return self.start == self.end
 
     @property
     def length(self) -> Real:
         return self.start - self.end
+
+    @property
+    def start_tuple(self) -> Tuple[Real, int]:
+        return self.start, 1 if self.start_open else 0
+
+    @property
+    def end_tuple(self) -> Tuple[Real, int]:
+        return self.end, 0 if self.end_closed else -1
 
     def __post_init__(self):
         if not isinstance(self.start, Real):
@@ -136,6 +136,105 @@ class Interval:
     def closed(self):
         return Interval(self.start, False, self.end, True)
 
+    def __apply_operator(self, func, other):
+        if isinstance(other, Real):
+            if self.is_degenerate:
+                return Interval(func(self.start, other), False, func(self.start, other), True)
+            else:
+                _start_tuple = min((func(self.start, other), self.start_open),
+                                   (func(self.end, other), self.end_open))
+                _end_tuple = max((func(self.start, other), self.start_closed),
+                                 (func(self.end, other), self.end_closed))
+                return Interval(*_start_tuple, *_end_tuple)
+
+        elif isinstance(other, Interval):
+            _start_tuple = min((func(self.start, other.start), self.start_open or other.start_open),
+                               (func(self.start, other.end), self.start_open or other.end_open),
+                               (func(self.end, other.start), self.end_open or other.start_open),
+                               (func(self.end, other.end), self.end_open or other.end_open))
+            _end_tuple = max((func(self.start, other.start), self.start_closed and other.start_closed),
+                             (func(self.start, other.end), self.start_closed and other.end_closed),
+                             (func(self.end, other.start), self.end_closed and other.start_closed),
+                             (func(self.end, other.end), self.end_closed and other.end_closed))
+            return Interval(*_start_tuple, *_end_tuple)
+
+        else:
+            raise TypeError
+
+    def __add__(self, other: Union[Real, 'Interval']):
+        if isinstance(other, Real):
+            return Interval(self.start + other, self.start_open, self.end + other, self.end_closed)
+
+        elif isinstance(other, Interval):
+            return Interval(self.start + other.start,
+                            self.start_open or other.start_open,
+                            self.end + other.end,
+                            self.end_closed and other.end_closed)
+
+        else:
+            raise TypeError
+
+    def __sub__(self, other: Union[Real, 'Interval']):
+        if isinstance(other, Real):
+            return Interval(self.start - other, self.start_open, self.end - other, self.end_closed)
+
+        elif isinstance(other, Interval):
+            return Interval(self.start - other.end,
+                            self.start_open or not other.end_closed,
+                            self.end - other.start,
+                            self.end_closed and not other.start_open)
+
+    def __mul__(self, other: Union[Real, 'Interval']):
+        if isinstance(other, Real):
+            return Interval(self.start * other, self.start_open, self.end * other, self.end_closed)
+
+        elif isinstance(other, Interval):
+            tmp = [self.start * other.start, self.start * other.end, self.end * other.start, self.end * other.end]
+            return Interval(min(tmp),
+                            self.start_open or other.start_open,  # todo: probably wrong
+                            max(tmp),
+                            self.end_closed and other.end_closed)  # todo: probably wrong
+
+        else:
+            raise TypeError
+
+    def reciprocal(self):
+        if 0 not in self:
+            return Interval(1 / self.end,
+                            not self.end_closed,  # todo: check
+                            1 / self.start,
+                            not self.start_open)  # todo: check
+
+        elif self.is_degenerate:
+            return Interval(-math.inf, False, math.inf, True)
+        elif self.start_tuple == (0, 0):
+            return Interval(1 / self.end,
+                            not self.end_closed,  # todo: check
+                            math.inf,
+                            True)
+        elif self.end_tuple == (0, 0):
+            return Interval(-math.inf,
+                            False,  # todo: check
+                            1 / self.start,
+                            not self.start_open)  # todo: check
+        else:
+            return Interval(-math.inf, False, math.inf, True)
+
+    def __truediv__(self, other):
+        if isinstance(other, Real):
+            if other > 0:
+                return Interval(self.start / other, self.start_open, self.end / other, self.end_closed)
+            elif other < 0:
+                return Interval(self.end / other, not self.end_closed, self.start / other, not self.start_open)
+            else:
+                Interval(-math.inf, False, math.inf, True)
+
+        elif isinstance(other, Interval):
+            return self * other.reciprocal()
+
+        else:
+            raise TypeError
+
     def __repr__(self):
         return (f'{self.__class__.__name__}('
                 f'{repr(self.start)}, '
@@ -162,12 +261,3 @@ class Interval:
             _right_bracket = ']' if self.end_closed and self.end != math.inf else ')'
 
         return f'{_left_bracket}{_start}, {_end}{_right_bracket}'
-
-
-if __name__ == '__main__':
-    print(Interval(2, False, 2, True))
-    print(Interval(1, False, 2, True))
-    print(Interval(1, True, 2, True))
-    print(Interval(1, True, 2, False))
-    print(Interval(1, False, 2, False))
-    print(Interval(-math.inf, False, 2, False))
